87047fe4bf402f000a525af6713736048e89d792
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-12-15 09:27:38 -0700

    Make minor fixes

--- src/basic_action_record_analysis.py
+++ src/basic_action_record_analysis.py
@@ -360,7 +360,7 @@ class CommandInformationSet:
         command_chain: CommandChain = CommandChain(None, [], chain)
         chain_target = min(len(record), chain + max_command_chain_considered)
         for chain_ending_index in range(chain, chain_target): 
-            if should_command_chain_not_cross_entry_at_record_index(record[chain_ending_index]): break
+            if should_command_chain_not_cross_entry_at_record_index(record, chain, chain_ending_index): break
             self.process_partial_chain_usage(record, command_chain)
         if verbose: print('chain', chain + 1, 'out of', len(record) - 1, 'target: ', chain_target - 1)
 
